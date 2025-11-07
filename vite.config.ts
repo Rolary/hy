30ca12a7acd6edd505e756d7894fcd5dd3497452
武宇敬 <wuyujing@weaver.com>
2025-11-07 22:39:42 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,20 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 function getPlugins() {
-  const plugins = [react(), tsconfigPaths()];
-  return plugins;
+    const plugins = [react(), tsconfigPaths()];
+    return plugins;
 }
 
 export default defineConfig({
-  plugins: getPlugins(),
+    plugins: getPlugins(),
+
+    server: {
+        host: true,
+        allowedHosts: ["seashell-app-umayw.ondigitalocean.app"],
+    },
+    preview: {
+        host: true,
+        port: 3000,
+        allowedHosts: ["seashell-app-umayw.ondigitalocean.app"],
+    },
 });
