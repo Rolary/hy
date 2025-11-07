4f5a2bf7695580c8fa94a3f0a046222469571f63
武宇敬 <wuyujing@weaver.com>
2025-11-07 22:54:48 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,3 @@
-/** WARNING: DON'T EDIT THIS FILE */
-/** WARNING: DON'T EDIT THIS FILE */
-/** WARNING: DON'T EDIT THIS FILE */
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -14,13 +10,24 @@ function getPlugins() {
 export default defineConfig({
     plugins: getPlugins(),
 
+    build: {
+        outDir: "dist/static",
+    },
+
     server: {
         host: true,
         allowedHosts: ["seashell-app-umayw.ondigitalocean.app"],
     },
+
+    // ✅ 告诉 vite preview 去 dist/static 找 index.html
     preview: {
         host: true,
         port: 3000,
         allowedHosts: ["seashell-app-umayw.ondigitalocean.app"],
+        open: false,
+        // 指定预览目录
+        // 注意：vite v5 以后这里是 `preview.root`
+        // 若你版本较旧，可用 `previewDir`
+        previewDir: "dist/static"
     },
 });
